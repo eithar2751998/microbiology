2458017fef3298da1f209c339eebb433897f45d0
eitharSaeed <eithar.saeed256@gmail.com>
2022-12-13 16:54:56 +0200

    commit

--- resources/views/dashboard/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Start Footerbar -->
-<div class="footerbar">
-    <footer class="footer">
-        <p class="mb-0"> &copy; 2022- All Rights Reserved.</p>
-    </footer>
-</div>
-<!-- End Footerbar -->
