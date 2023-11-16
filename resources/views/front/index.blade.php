403291ce65258f643200daf5eba76d778f12bd54
eitharSaeed <eithar.saeed256@gmail.com>
2023-11-16 17:25:31 +0200

    updates

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -8,84 +8,61 @@
     <div class="main-content">
         <!-- Slider Section Start -->
         <div class="rs-slider main-home">
-            <div class="rs-carousel owl-carousel" data-loop="true" data-items="1" data-margin="0" data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="false" data-nav="false" data-nav-speed="false" data-center-mode="false" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false" data-ipad-device="1" data-ipad-device-nav="false" data-ipad-device-dots="false" data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="false" data-md-device="1" data-md-device-nav="true" data-md-device-dots="false">
+            <div class="rs-carousel owl-carousel" data-loop="true" data-items="6" data-margin="0" data-autoplay="true"
+                 data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="false" data-nav="false" data-nav-speed="false"
+                 data-center-mode="false" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false" data-ipad-device="1"
+                 data-ipad-device-nav="false" data-ipad-device-dots="false" data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="false"
+                 data-md-device="1" data-md-device-nav="true" data-md-device-dots="false">
                 <div class="slider-content slide1">
                     <div class="container">
                         <div class="content-part">
-                            <div class="sl-sub-title wow bounceInLeft" data-wow-delay="300ms" data-wow-duration="2000ms">Start to learning today</div>
-                            <h1 class="sl-title wow fadeInRight" data-wow-delay="600ms" data-wow-duration="2000ms">Online Courses From Leading Experts</h1>
                             <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
-                                <a class="readon orange-btn main-home" href="{{route('front.course.index')}}">Find Courses</a>
-                            </div>
+<a class="readon orange-btn main-home float-left" style="margin-top: 250px" href="{{route('front.course.index')}}">Find Courses</a>                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="slider-content slide2">
                     <div class="container">
                         <div class="content-part">
-                            <div class="sl-sub-title wow bounceInLeft" data-wow-delay="300ms" data-wow-duration="2000ms">Start to learning today</div>
-                            <h1 class="sl-title wow fadeInRight" data-wow-delay="600ms" data-wow-duration="2000ms">Explore Interests and Career With Courses</h1>
                             <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
-                                <a class="readon orange-btn main-home" href="{{route('front.course.index')}}">Find Courses</a>
+                                <a class="readon orange-btn main-home float-left" style="margin-top: 250px" href="{{route('front.course.index')}}">Find Courses</a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- Features Section start -->
-            <div id="rs-features" class="rs-features main-home">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-12 md-mb-30">
-                            <div class="features-wrap">
-                                <div class="icon-part">
-                                    <img src="{{asset('assets/front/images/features/icon/3.png')}}" alt="">
-                                </div>
-                                <div class="content-part">
-                                    <h4 class="title">
-                                        <span class="watermark">5,320 online courses</span>
-                                    </h4>
-                                    <p class="dese">
-                                        Enjoy a variety of fresh topics
-                                    </p>
-                                </div>
-                            </div>
+                <div class="slider-content slide3">
+                    <div class="container">
+                        <div class="content-part">
+                            <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
+<a class="readon orange-btn main-home float-left" style="margin-top: 250px" href="{{route('front.course.index')}}">Find Courses</a>                            </div>
                         </div>
-                        <div class="col-lg-4 col-md-12 md-mb-30">
-                            <div class="features-wrap">
-                                <div class="icon-part">
-                                    <img src="{{asset('assets/front/images/features/icon/2.png')}}" alt="">
-                                </div>
-                                <div class="content-part">
-                                    <h4 class="title">
-                                        <span class="watermark">Expert instruction</span>
-                                    </h4>
-                                    <p class="dese">
-                                        Find the right instructor
-                                    </p>
-                                </div>
-                            </div>
+                    </div>
+                </div>
+                <div class="slider-content slide4">
+                    <div class="container">
+                        <div class="content-part">
+                            <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
+<a class="readon orange-btn main-home float-left" style="margin-top: 250px" href="{{route('front.course.index')}}">Find Courses</a>                            </div>
                         </div>
-                        <div class="col-lg-4 col-md-12">
-                            <div class="features-wrap">
-                                <div class="icon-part">
-                                    <img src="{{asset('assets/front/images/features/icon/1.png')}}" alt="">
-                                </div>
-                                <div class="content-part">
-                                    <h4 class="title">
-                                        <span class="watermark">Lifetime access</span>
-                                    </h4>
-                                    <p class="dese">
-                                        Learn on your schedule
-                                    </p>
-                                </div>
-                            </div>
+                    </div>
+                </div>
+                <div class="slider-content slide5">
+                    <div class="container">
+                        <div class="content-part">
+                            <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
+<a class="readon orange-btn main-home float-left" style="margin-top: 250px" href="{{route('front.course.index')}}">Find Courses</a>                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-content slide6">
+                    <div class="container">
+                        <div class="content-part">
+                            <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
+<a class="readon orange-btn main-home float-left" style="margin-top: 250px" href="{{route('front.course.index')}}">Find Courses</a>                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- Features Section End -->
         </div>
         <!-- Slider Section End -->
 
